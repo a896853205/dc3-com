ed8494a1621cf3c6501607f1b50ef83160c8c6ad
任涵彬 <1870012027@qq.com>
2021-01-05 17:11:41 +0800

    'feat:增加echarts图表'

--- src/page/capsule/components/outter/index.tsx
+++ src/page/capsule/components/outter/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-
 import BarChart from '../bar-chart';
+import CalendarHeadmapChart from '../calendar-heatmap-chart';
+import EncodeChart from '../encode-chart';
+import GradientMappingChart from '../gradient-mapping-chart';
+import RadarChart from '../radar-chart';
 import './style.css';
 
 export default () => (
-  <div className='outter-container'>
-    <div className='col12 item-box'>
+  <div className="outter-container">
+    <div className="col12 item-box">
       <BarChart />
     </div>
-    <div className='item-box'>映射为渐变色</div>
-    <div className='item-box'>AQI 雷达图</div>
-    <div className='col12 item-box'>3d Scatter with Scatter Matrix</div>
-    <div className='col12 item-box'>日历热力图</div>
+    <div className="item-box">
+      <GradientMappingChart />
+    </div>
+    <div className="item-box">
+      <RadarChart />
+    </div>
+    <div className="col12 item-box">
+      <EncodeChart />
+    </div>
+    <div className="col12 item-box">
+      <CalendarHeadmapChart />
+    </div>
   </div>
 );
