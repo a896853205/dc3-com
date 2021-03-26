d7a5b8c909a31ec84aa21152fc450e04dc5f4525
renhanbin <1870012027@qq.com>
2021-03-26 17:56:27 +0800

    feat:添加首页内容

--- src/page/timeline/components/timeline.tsx
+++ src/page/timeline/components/timeline.tsx
@@ -1,39 +1,33 @@
 import React from 'react';
 
-import { Timeline } from 'antd';
-import { ClockCircleOutlined } from '@ant-design/icons';
+import { Image } from 'antd';
+
 import Breadcrumb from 'src/components/Breadcrumb/Breadcrumb';
 
-import Content from './content';
+// import Content from './content';
 
 export default () => {
-  const description = [
-    '设备接入模块可方便快捷的实现设备的接入与配置，我们提供可视化的配置模式，方便用户接入设备',
-    '设备数据管理模块主要进行数据的管理，其主要分为3个部分：1. 采集参数设置 2. 实时数据查看 3. 历史数据管理',
-    '状态监控模块主要用于数据的可视化展示，其主要分为2个部分：1. 设备状态监控 2. 平台状态监控 ',
-    '告警管理模块主要用于实现告警数据的展示和管理，其主要分为两个部分：1. 告警模式设置 2. 告警数据管理',
-    '用户管理模块主要用于用户数据的展示和管理',
-  ];
   return (
     <>
       <Breadcrumb />
-      <Timeline>
-        <Timeline.Item dot={<ClockCircleOutlined />}>
-          <Content title='设备接入' description={description[0]} />
-        </Timeline.Item>
-        <Timeline.Item dot={<ClockCircleOutlined />}>
-          <Content title='设备数据管理' description={description[1]} />
-        </Timeline.Item>
-        <Timeline.Item dot={<ClockCircleOutlined />}>
-          <Content title='状态监控' description={description[2]} />
-        </Timeline.Item>
-        <Timeline.Item dot={<ClockCircleOutlined />}>
-          <Content title='告警管理' description={description[3]} />
-        </Timeline.Item>
-        <Timeline.Item dot={<ClockCircleOutlined />}>
-          <Content title='用户管理' description={description[4]} />
-        </Timeline.Item>
-      </Timeline>
+      <Image width={1000} src={require('../pic/dc3.png')} />
+      <ol>
+        <li>
+          当驱动启动时，本平台就可以采集到所有已经启动的驱动的信息。点击模板，点击新建模板。通过给模板分配驱动，实现模板和驱动的连接。
+        </li>
+        <li>
+          点击位号，通过给新建的位号分配模板，实现位号和驱动的连接。这个位号其实就代表一个数据类型，并没有实际的意义。
+        </li>
+        <li>
+          点击驱动配置，就可以给第一步中新建的模板进行配置。例如我们在第一步中建立的模板对应的驱动是modbus驱动，那么在这一步我们就可以按照modbus驱动对应的属性名，配置属性值。
+        </li>
+        <li>
+          点击设备，在新建设备的时候，通过给设备配置模板，实现模板和设备的连接。
+        </li>
+        <li>
+          点击位号配置，因为对应的设备不同，所以需要填所属设备，因为位号对应的驱动不同，所以属性名的下拉菜单会根据驱动类型动态变化，内容就是属性值。比如我们选的设备是温湿度传感器，给它选的位号对应的驱动是modbus驱动。所以它的属性有三个分别是偏移量、从站编号和功能码。我们可以从这三个属性的属性值找到这个温度传感器的温度值。
+        </li>
+      </ol>
     </>
   );
 };
